fa196ca565056466440c22070e3d62cff12fbeac
Chaitya0623 <91510927+Chaitya0623@users.noreply.github.com>
2022-08-19 18:18:50 +0530

    Updated .js file

--- guessingGame.js
+++ guessingGame.js
@@ -5,24 +5,24 @@ while (!maximum) {
 
 const targetNum = Math.floor(Math.random()*maximum) + 1;
 
-let guess = parseInt(promp("Enter your first guess!"));
+let guess = parseInt(prompt("Enter your first guess!"));
 let attempts = 1;
 
-while (parseInt(guess !== targetNum)) {
+while (parseInt(guess) !== targetNum) {
     if (guess === 'q') {
         break;
     }
     attempts++;
     if (guess > targetNum) {
-        prompt("Too high! Enter a new guesss");
+        guess = prompt("Too high! Enter a new guess");
     } else {
-        prompt("Too low! Enter a new guesss");
+        guess = prompt("Too low! Enter a new guess");
     }
 }
 
 if (guess === 'q') {
     console.log("OK, YOU QUIT");
 } else {
-    console.log("Congrats You Win")
+    console.log("Congrats You Win");
     console.log(`YOU GOT IT! It took you ${attempts} guesses`);
 }
